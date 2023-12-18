099f728fd5434ccd3ad10cb33fb511203f4f763e
Larkin Campbell <Larkincampbell24@gmail.com>
2023-12-17 16:15:02 -0800

    step 3 complete

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import pizza from './images/pizza.jpg'
+import { useNavigate } from 'react-router-dom'
+import Form from './Form'
+
+
+
 
 function Home() {
+
+  const navigate = useNavigate()
+
+  const goHome = () => {
+    navigate('/order')
+  }
   return (
     <div>
       <h2>
         Welcome to Bloom Pizza!
       </h2>
-      {/* clicking on the img should navigate to "/order" */}
-      <img alt="order-pizza" style={{ cursor: 'pointer' }} src={pizza} />
+      <img onClick={goHome} alt="order-pizza" style={{ cursor: 'pointer' }} src={pizza} />
     </div>
   )
 }
